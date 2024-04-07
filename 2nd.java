3cba15d543004a57b7f0645bc1abff37a5cbbf4f
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-07 21:41:18 +0530

    2nd.java

--- 2nd.java
+++ 2nd.java
@@ -1,7 +1,7 @@
 //jump to new line "/n"
 class Example {
     public static void main(String[] args){
-        System.out.print("Hello World./n")
-        System.out.print("Sri Lanka.")
+        System.out.print("Hello World./n");
+        System.out.print("Sri Lanka.");
     }
 }
