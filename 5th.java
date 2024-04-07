6b05681766aa3b2ba2ac3ef51dc3f4d5f5c8c34f
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-07 21:54:31 +0530

    5th.java

--- 5th.java
+++ 5th.java
@@ -1,5 +1,9 @@
+// mathematical operation
 public class Main {
     public static void main(String[] args) {
+      System.out.println(2 + 5);
+      System.out.println(2 - 5);
       System.out.println(2 * 5);
+      System.out.print(2 / 5);  
     }
   }
