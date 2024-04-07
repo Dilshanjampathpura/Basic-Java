2d374c9500d46d3aade633434015383e5e3d7aac
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-07 21:56:29 +0530

    5th.java

--- 5th.java
+++ 5th.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println(2 + 5);
-      System.out.println(2 - 5);
-      System.out.println(2 * 5);
-      System.out.print(2 / 5);  
+      System.out.println(7 - 6);
+      System.out.println(2 * 3);
+      System.out.print(9 / 3);  
     }
   }
