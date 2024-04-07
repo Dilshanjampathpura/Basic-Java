a3444c2874889016886347530c6eb8d7e52c1c11
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-07 21:41:44 +0530

    3rd.java

--- 3rd.java
+++ 3rd.java
@@ -1,7 +1,7 @@
 //jum to new line "println" statement
 class Example {
     public static void main(String[] args){
-        System.out.println("Hello World.")
-        System.out.println("Sri Lanka.")
+        System.out.println("Hello World.");
+        System.out.println("Sri Lanka.");
     }
 }
