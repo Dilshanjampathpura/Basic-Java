8ae0c73873d47cd38da51332c47d4d7493d0f442
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-08 14:51:43 +0530

    Add files via upload

---
+++ 7th.java
@@ -0,0 +1,20 @@
+public class VariableTypesExample {
+    
+    public static void main(String[] args) {
+        
+        int INTEGER;
+        double FLOAT;
+        String STRING;
+        boolean BOOLEAN;
+        
+        INTEGER = 30;
+        FLOAT = 9.81;
+        STRING = "Static variable";
+        BOOLEAN = false;
+
+        System.out.println("Integer variable: " + INTEGER);
+        System.out.println("Float variable: " + FLOAT);
+        System.out.println("String variable: " + STRING);
+        System.out.println("Boolean variable: " + BOOLEAN);
+    }
+}
