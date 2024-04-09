f8f73f9d68c522628b8897d21d15fa0fec2d1ef6
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-09 16:39:12 +0530

    Add files via upload

---
+++ 12th.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+      int marks = 4;
+      switch (marks) {
+        case 1:
+          System.out.println("very poor");
+          break;
+        case 2:
+          System.out.println("poor");
+          break;
+        case 3:
+          System.out.println("normal");
+          break;
+        case 4:
+          System.out.println("good");
+          break;
+        case 5:
+          System.out.println("very good");
+          break;
+      }
+    }
+  }
