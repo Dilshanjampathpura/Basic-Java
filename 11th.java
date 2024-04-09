1a6277f8060cb9ee7d56a83f51fbd782819e6898
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-09 16:24:37 +0530

    Add files via upload

---
+++ 11th.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+      int x , y;
+      x = 30;
+      y = 20;
+      if (x < y) {
+        System.out.println("y is grater than x.");
+      } 
+      else if (x > y) {
+        System.out.println("x is grater than y.");
+      }  
+      else {
+        System.out.println("x and y are equal numbers.");
+      }
+    }
+  }
+  
