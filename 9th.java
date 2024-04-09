ce47902f32985001db10cb328080ee8afe1845e6
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-09 15:37:39 +0530

    9th.java

--- 9th.java
+++ 9th.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-      System.out.println(Math.max(48, 34)); //select highest value
-      System.out.println(Math.min(48, 34));  //select lowest value
-      System.out.println(Math.sqrt(36)); //give square root of value
+        System.out.println(Math.max(48, 34)); //select highest value
+        System.out.println(Math.min(48, 34));  //select lowest value
+        System.out.println(Math.sqrt(36)); //give square root of value
+        System.out.println(Math.abs(-12)); //give absolute value
+        
+        int x = (int)(Math.random() * 21);  // select random value between 0 to 20
+        System.out.println(x);
     }
   }
