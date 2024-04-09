fdfc2eb920dcf53248359304b883f841733dbfef
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-09 15:26:42 +0530

    Add files via upload

---
+++ 9th.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(Math.max(48, 34)); //select highest value
+      System.out.println(Math.min(48, 34));  //select lowest value
+      System.out.println(Math.sqrt(36)); //give square root of value
+    }
+  }
