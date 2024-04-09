8170b3a9523877f9ee3d9d90b554828a4524f4c7
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-09 14:57:32 +0530

    Add files via upload

---
+++ 8th.java
@@ -0,0 +1,29 @@
+public class Main {
+  public static void main(String[] args) {
+    float x, y, z;
+    x = y = z = 50;
+    System.out.println(x + y + z);
+    x = 20;
+    y = 30;
+    z = 40;
+    System.out.println(x + y + z);
+    x = 25;
+    y = 10;
+    z = 5;
+    System.out.println(x - y - z);
+    x = 5;
+    y = 3;
+    z = 4;
+    System.out.println(x * y * z);
+    x = 2;
+    y = 3;
+    z = 4;
+    System.out.println(x * y / z);
+    x = 10;
+    y = 4;
+    System.out.println(x / y);
+    x = 10;
+    y = 4;
+    System.out.println(x % y);
+  }
+}
