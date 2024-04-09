b6c766e1f594243bc193c9e018f5472addd29e09
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-09 15:50:15 +0530

    Add files via upload

---
+++ 10th.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      int x = 25;
+      int y = 18;
+      System.out.println(x > y); // returns true
+      System.out.println(x < y); // returns false 
+    }
+  }
+  
