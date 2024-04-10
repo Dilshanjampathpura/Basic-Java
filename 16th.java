250215b85f1dd5cb277bcf2de9faa293b75291c3
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-10 16:50:14 +0530

    Add files via upload

---
+++ 16th.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      String[] english = {"reding", "writing", "listning", "speaking"};
+      System.out.println(english[0]);
+      System.out.println(english[1]);
+      System.out.println(english[2]);
+      System.out.println(english[3]);
+      System.out.println(english.length);
+    }
+}
+  
