e64832ed90f39faa2c0058caff7f9ba4d4563fbb
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-10 16:13:34 +0530

    Add files via upload

---
+++ 13th.java
@@ -0,0 +1,11 @@
+// while loop
+public class Main {
+    public static void main(String[] args) {
+      int x = 0;
+      while (x < 11) {
+        System.out.println(x);
+        x = x+1;
+      }  
+    }
+}
+  
---
+++ 14th.java
@@ -0,0 +1,12 @@
+//do while loop
+public class Main {
+    public static void main(String[] args) {
+      int x = 0;
+      do {
+        System.out.println(x);
+        x = x+1;
+      }
+      while (x < 11);  
+    }
+}
+  
