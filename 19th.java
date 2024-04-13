79861b2c5db43ca4df3de6213086eff251cd1013
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-13 12:16:43 +0530

    Add files via upload

---
+++ 19th.java
@@ -0,0 +1,11 @@
+public class Main {
+    static void myMethod() {
+      System.out.println("I am Lahiru Dilshan Jampathpura");
+    }
+  
+    public static void main(String[] args) {
+      myMethod();
+      myMethod();
+    }
+  }
+  
