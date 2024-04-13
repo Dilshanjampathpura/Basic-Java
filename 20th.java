9f4b0158a055fb43e89e0a9ae3ee9e1ba7cab045
Dilshanjampathpura <165460681+Dilshanjampathpura@users.noreply.github.com>
2024-04-13 12:28:50 +0530

    Add files via upload

---
+++ 20th.java
@@ -0,0 +1,12 @@
+public class Main {
+    static void university(String uniname) {
+      System.out.println("University of " + uniname);
+    }
+  
+    public static void main(String[] args) {
+      university("Colombo");
+      university("Jaffna");
+      university("Rajarata");
+      university("Moratuwa");
+    }
+}
